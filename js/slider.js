fa305be97d91cd121f51c423b78d90f937e9fbf2
scurimich <tsykota.vlad@gmail.com>
2019-04-23 21:44:15 +0300

    fix adaptive sliders

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,41 @@
 $(function() {
-    $(document).on('click', '.slider-nav-button', function(e) {
-        if ($(this).is('.active')) return;
+  var adaptiveWidth = 1190;
 
-        var activeButton = $(this).siblings('.active');
-        var activeButtonIndex = $(this).siblings('.active').index();
-        var thisIndex = $(this).index();
+  $(document).on('click', '.slider-nav-button', function(e) {
+    if ($(this).is('.active')) return;
 
+    var activeButton = $(this).siblings('.active');
+    var activeButtonIndex = $(this).siblings('.active').index();
+    var thisIndex = $(this).index();
 
-        var nav = $(this).parents('.slider-nav');
-        var sliderId = nav.attr('data-slider');
+    var nav = $(this).parents('.slider-nav');
+    var sliderId = nav.attr('data-slider');
+    
+    var windowWidth = $(window).outerWidth();
+    var slider = $('[data-slides='+ sliderId +']');
+    var adaptiveSlider = slider.attr('data-adapt');
+    var adaptive = windowWidth < adaptiveWidth;
+    console.log(adaptive, adaptiveSlider);
+    if (!adaptiveSlider && adaptive) return;
 
-        var slider = $('[data-slides='+ sliderId +']');
-        var slideList = slider.find('.slides');
-        if (slideList.hasClass('transitioning')) return;
-        slideList.off('transitionend webkitTransitionEnd oTransitionEnd');
+    var slideList = slider.find('.slides');
+    if (slideList.hasClass('transitioning')) return;
+    slideList.off('transitionend webkitTransitionEnd oTransitionEnd');
 
-        var slidesPosition = slideList.css('left');
-        var slide = slider.find('.slide');
-        var slidesNumber = slider.attr('data-number');
+    var slidesPosition = slideList.css('left');
+    var slide = slider.find('.slide');
+    var dataNumber = slider.attr('data-number');
+    if (!adaptive && !dataNumber) return;
+    var slidesNumber = adaptive ? 1 : dataNumber;
 
-        activeButton.removeClass('active');
-        $(this).addClass('active');
+    activeButton.removeClass('active');
+    $(this).addClass('active');
 
-        var slidesWidth = (slide.outerWidth() * slidesNumber) + (parseInt(slide.css('margin-right')) * slidesNumber);
-        slideList.css('left', parseInt(slidesPosition) + (activeButtonIndex - thisIndex) * slidesWidth + 'px');
-        slideList.addClass('transitioning');
-        slideList.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
-            $(this).removeClass('transitioning');
-        });
+    var slidesWidth = (slide.outerWidth() * slidesNumber) + (parseInt(slide.css('margin-right')) * slidesNumber);
+    slideList.css('left', parseInt(slidesPosition) + (activeButtonIndex - thisIndex) * slidesWidth + 'px');
+    slideList.addClass('transitioning');
+    slideList.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+      $(this).removeClass('transitioning');
     });
+  });
 });
