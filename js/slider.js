d576c24643a8ea8292dbb48514f6a4281949f672
scurimich <tsykota.vlad@gmail.com>
2019-04-23 11:18:46 +0300

    fix desktop

--- js/slider.js
+++ js/slider.js
@@ -6,20 +6,27 @@ $(function() {
         var activeButtonIndex = $(this).siblings('.active').index();
         var thisIndex = $(this).index();
 
-        activeButton.removeClass('active');
-        $(this).addClass('active');
 
         var nav = $(this).parents('.slider-nav');
         var sliderId = nav.attr('data-slider');
 
         var slider = $('[data-slides='+ sliderId +']');
         var slideList = slider.find('.slides');
+        if (slideList.hasClass('transitioning')) return;
+        slideList.off('transitionend webkitTransitionEnd oTransitionEnd');
+
         var slidesPosition = slideList.css('left');
         var slide = slider.find('.slide');
         var slidesNumber = slider.attr('data-number');
 
-        var slidesWidth = (slide.outerWidth() * slidesNumber) + (parseInt(slide.css('margin-right')) * slidesNumber);
+        activeButton.removeClass('active');
+        $(this).addClass('active');
 
+        var slidesWidth = (slide.outerWidth() * slidesNumber) + (parseInt(slide.css('margin-right')) * slidesNumber);
         slideList.css('left', parseInt(slidesPosition) + (activeButtonIndex - thisIndex) * slidesWidth + 'px');
+        slideList.addClass('transitioning');
+        slideList.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+            $(this).removeClass('transitioning');
+        });
     });
 });
